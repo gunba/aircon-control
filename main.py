523a123b4bee0eb73859f357edfea02d63610660
gunba <jordan@gunba.org>
2024-06-12 06:17:41 +0800

    Validation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from airtouch4pyapi import AirTouch
 import logging
 import os
 from datetime import datetime
 import asyncio
+from airtouch4pyapi import AirTouch
 
 # Get the directory of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -25,8 +25,11 @@ async def updateInfoAndDisplay(ip):
         # Identify main bedroom (we won't manage other rooms)
         room = next((group for group in groups if group.GroupName == "Main Bed"), None)
 
-        # Calculate correct fan open ratio
-        flooms_open_ratio = (room.Temperature - 22.8)
+        # Lower the target temperature by 0.5 degrees
+        target_temperature = 22.5
+
+        # Calculate correct fan open ratio based on the new target temperature
+        flooms_open_ratio = (room.Temperature - target_temperature)
         flooms_open_ratio = max(flooms_open_ratio, 0.05)
         flooms_open_ratio = min(flooms_open_ratio, 1)
         flooms_open_ratio = flooms_open_ratio * 100
@@ -34,13 +37,13 @@ async def updateInfoAndDisplay(ip):
 
         if flooms_open_ratio < 20:
             await at.TurnAcOff(0)
-            logging.info("Turning AC off")
+            logging.info(f"Turning AC off. Current Room Temperature: {room.Temperature}")
         else:
             if acs[0].IsOn == "Off":
                 await at.TurnAcOn(0)
                 logging.info("Turning AC on")
-            # Update room temperature
-            await at.SetGroupToTemperatureByGroupName(room.GroupName, 23)
+            # Update room temperature and fan open ratio
+            await at.SetGroupToTemperatureByGroupName(room.GroupName, target_temperature)
             await at.SetGroupToPercentByGroupName(room.GroupName, flooms_open_ratio)
             logging.info(f"Room Temperature: {room.Temperature}")
             logging.info(f"Fan Open Ratio: {flooms_open_ratio}")
